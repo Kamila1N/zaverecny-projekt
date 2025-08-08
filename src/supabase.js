1a1302125771f6f50abe0c7e62bcd633cd73f0b9
Kamila1N <novotna.kama@seznam.cz>
2025-08-08 15:29:22 +0200

    supabese uprava

--- src/supabase.js
+++ src/supabase.js
@@ -1,4 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
 export const supabase = createClient(
-    "https://jtwncyfyacesipxhnnrj.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp0d25jeWZ5YWNlc2lweGhubnJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMyMDExNjIsImV4cCI6MjA2ODc3NzE2Mn0.j7qztI0BIZ8c4SJ7fwZXHjzwKsrYD49m9YUTlKKiTa4");
+    import.meta.env.VITE_SUPABASE_URL,
+    import.meta.env.VITE_SUPABASE_ANON_KEY)
